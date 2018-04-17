8257a52311d2d00568af4cadd7e10ff098a0f024
26bee <26bee.gmail.com>
2018-04-17 08:18:56 +0700

    Implement init

---
+++ src/instruction_list.c
@@ -0,0 +1,20 @@
+#ifndef INSTRUCTION_LIST
+#define INSTRUCTION_LIST
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Instruction {
+	char* init_state;
+	char read_symbol;
+	char* new_state;
+	char new_symbol;
+	char motion;
+	int num;
+	struct Instruction* next;
+	struct Instruction* prev;
+};
+
+int init_instruction(struct Instruction* head);
+#endif
