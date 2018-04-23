dec0ea3b2885b388092d402634465ad6b733bc34
Falconarhia <kta.11@mail.ru>
2018-04-23 11:23:43 +0700

    Add init func

---
+++ src/tmi.h
@@ -0,0 +1,18 @@
+#ifndef TMI_H
+#define TMI_H
+
+#include <stdio.h>
+#include "mt.h"
+#include "double_linked_list.h"
+#include "parcer.h"
+
+#define SIZE_STR 100
+
+struct TMI {
+	struct MT mt;
+	struct DLL_Node instruction;
+	struct DLL_Node breakpoint;
+};
+
+int init_tmi(struct TMI* tmi, size_t len_mtape, const char* filename);
+#endif
