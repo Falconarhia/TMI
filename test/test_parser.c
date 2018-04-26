7b2e8279caf8749f31ccf68ca8d1acf3918df810
Falconarhia <kta.11@mail.ru>
2018-04-26 18:19:55 +0700

    Add parser tests

--- test/test_parser.c
+++ test/test_parser.c
@@ -16,7 +16,37 @@ CTEST(IS_PRINTED_CHAR, ok) {
 CTEST(PARSER, parser_fail) {
 	int a;
 	int expected;
-	a = parser(NULL, "start 1 start 0 r");
+	a = parser_instruction(NULL, "start 1 start 0 r");
 	expected = -1;
+	ASSERT_EQUAL(expected, a);
+
+	struct Instruction head;
+	init_instruction(&head);
+
+	a = parser_instruction(&head, NULL);
+	expected = -2;
+	ASSERT_EQUAL(expected, a);
+
+	a = parser_instruction(&head, "");
+	expected = -2;
+	ASSERT_EQUAL(expected, a);
+
+	a = parser_instruction(&head, "asdadqwd");
+	expected = -3;
+	ASSERT_EQUAL(expected, a);
+}
+
+CTEST(PARSER, parser_ok) {
+	int a;
+	int expected;
+	struct Instruction head;
+	init_instruction(&head);
+
+	a = parser_instruction(&head, "start 1 start 0 r");
+	expected = 0;
+	ASSERT_EQUAL(expected, a);
+
+	a = parser_instruction(&head, "start 1 start 0 l!");
+	expected = 1;
 	ASSERT_EQUAL(expected, a);
 }
